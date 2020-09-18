5c2501cee4679edf266564b26393b1b8e986138a
dj-dedek <dedekitsjay@gmail.com>
2020-09-18 14:18:35 +0700

    clone trello with redux

---
+++ src/components/App.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import TrelloList from './TrelloList';
+import { connect } from 'react-redux';
+import TrelloActionButton from './TrelloActionButton';
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
+import { sort } from './../actions';
+import styled from 'styled-components';
+
+const ListsContainer = styled.div `
+  display: flex;
+  flex-direction: row;
+`
+
+class App extends Component {
+
+  onDragEnd = (result) => {
+    const { destination, source, draggableId, type } = result;
+
+    if(!destination) {
+      return;
+    }
+
+    this.props.dispatch(
+      sort(
+        source.droppableId,
+        destination.droppableId,
+        source.index,
+        destination.index,
+        draggableId,
+        type
+      )
+    )
+  }
+
+  render() {
+    const { lists } = this.props
+
+      return (
+        <DragDropContext onDragEnd={this.onDragEnd}>
+          <div>
+            <h1>Hello World</h1>
+            <Droppable droppableId="all-lists" direction="horizontal" type="list">
+              {provided => (
+                <ListsContainer 
+                  {...provided.droppableProps} 
+                  ref={provided.innerRef}
+                >
+                  {
+                    lists.map((list, index) => 
+                      <TrelloList 
+                        listID={list.id} 
+                        key={list.id} 
+                        title={list.title} 
+                        cards={list.cards} 
+                        index={index}
+                      />
+                    )
+                  }
+                  {provided.placeholder}
+                  <TrelloActionButton list />
+                </ListsContainer>
+              )}
+            </Droppable>
+          </div>
+      </DragDropContext>
+    );
+  }
+}
+
+// eslint-disable-next-line
+const styles = {
+  listsContainer: {
+    display: "flex",
+    flexDirection: "row",
+
+  }
+}
+
+const mapStateToProps = state => ({
+  lists: state.lists
+})
+
+export default connect(mapStateToProps)(App);
